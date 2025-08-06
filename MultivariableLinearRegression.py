57951d03ff22bdf55acf2b8ed08678a48da28b06
Avi-1029 <ava.xz80@gmail.com>
2025-08-06 10:26:25 +0400

    Update MultivariableLinearRegression.py

--- MultivariableLinearRegression.py
+++ MultivariableLinearRegression.py
@@ -4,13 +4,15 @@ import numpy as np
 boston = pd.read_csv(r"C:\Users\Biju\Desktop\Jetlearn\ML and AI\Datasets\HousingData.csv")
 print(boston.info())
 
+boston = boston[["RM", "LSTAT", "MEDV"]]
+
+#Removing the missing data
+boston.dropna(inplace = True)
+print(boston.isnull().sum())
+
 #Preprocessing data:
 X = boston[["RM" , "LSTAT"]]
 y = boston["MEDV"]
-print(X.isnull().sum())
-
-#Removing the missing data
-X.dropna(inplace = True)
 print(X.isnull().sum())
 
 #Spliting the data (Training and testing)
@@ -21,3 +23,20 @@ print(xtrain)
 print(xtest)
 print(ytrain)
 print(ytest)
+
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+
+model.fit(xtrain, ytrain)
+
+predict = model.predict(xtest)
+print(predict)
+
+from sklearn.metrics import root_mean_squared_error
+
+error = root_mean_squared_error(ytest, predict)
+print(error)
+print("Gradient = ", model.coef_, ", Intercept = ", model.intercept_)
+
+
