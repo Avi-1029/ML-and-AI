15453f386b93cab7c34ec9bdc2a6aa522b61fc93
Avi-1029 <ava.xz80@gmail.com>
2025-09-26 19:11:29 +0400

    Update RandomForestClassifier.py

--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 data = pd.read_csv(r"C:\Users\Biju\Desktop\Jetlearn\ML and AI\Datasets\adult_income.csv", sep = ", ")
 print(data.info())
 print(data)
@@ -14,6 +15,8 @@ for i in list:
     data[i] = encoder.fit_transform(data[i])
 #print(data.info())
 
+#print(data[["education","educational-num"]])
+
 #Finding the corelation between features and target:
 cormatrix = data.corr()
 print(cormatrix)
@@ -21,5 +24,25 @@ sns.heatmap(cormatrix, annot = True)
 plt.show()
 
 #Deciding features nd target:
-X = data[[]]
+X = data[["age", "workclass", "educational-num", "marital-status" ,"occupation", "relationship" ,"gender", "race" ,"hours-per-week"]]
+y = data["income"]
 
+#Oversampling to balance the data
+from imblearn.over_sampling import RandomOverSampler
+sampler = RandomOverSampler(sampling_strategy= "auto", random_state=24)
+resampledx, resampledy = sampler.fit_resample(X, y)
+
+#Splitting the data:
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(resampledx, resampledy, test_size = 0.3, random_state= 24)
+
+#Initiating the model
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier()
+model.fit(xtrain, ytrain)
+prediction = model.predict(xtest)
+print(prediction)
+
+#Accuracy of the model
+from sklearn.metrics import classification_report
+print(classification_report(ytest, prediction))
